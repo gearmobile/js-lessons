ca5dbd973ac02cf57d676263c17b4a697b5d5249
gearmobile <gearmobile@gmail.com>
2016-10-10 19:43:00 +0300

    js-lessons

--- javascript/callback-function/app.js
+++ javascript/callback-function/app.js
@@ -2,45 +2,52 @@ window.addEventListener( 'load', function () {
     function functionName() {
         // some code
     }
+
     //
     function functionName( parameter1, parameter2 ) {
         // some code
     }
     functionName( argument1, argument2 );
+
     //
-    function functionOne(x) { return x; }
+    function functionOne(x) { console.log(x); }
     function functionTwo( parameter1 ) {
         // some code
     }
     functionTwo( functionOne );
+
     //
-    function functionOne(x) { alert(x); }
+    function functionOne(x) { console.log(x); }
     function functionTwo( parameter1, callback ) {
         callback( parameter1 );
     }
     functionTwo( 2, functionOne );
+
     //
     function functionTwo( parameter1, callback ) {
         callback( parameter1 );
     }
-    functionTwo(1, function (x) { alert(x); });
+    functionTwo(1, function (x) { console.log(x); });
+
     //
     function functionTwo( parameter1, callback ) {
         callback( parameter1 );
         callback( parameter1 );
     }
-    functionTwo( 1, function (x) { alert(x); } );
+    functionTwo( 1, function (x) { console.log(x); } );
+
     //
     function functionTwo( parameter1, parameter2, callback1, callback2 ) {
         callback1( parameter1 );
         callback2( parameter2 );
     }
-    functionTwo( 1, 2, function (x) { alert(x); }, function (x) { alert(x); } );
+    functionTwo( 1, 2, function (x) { console.log(x); }, function (x) { console.log(x); } );
+
     //
     function functionTwo( parameter1, callback1, callback2 ) {
         callback1( parameter1 );
         callback2( parameter1 );
     }
     functionTwo( 2, functionOne, functionOne );
-    functionTwo( 2, functionOne, function(x) { alert( x + 2 ); });
+    functionTwo( 2, functionOne, function(x) { console.log( x + 2 ); });
 }, false);
