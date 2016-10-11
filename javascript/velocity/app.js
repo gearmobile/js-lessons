50fd5198bd5c0ff7565e505a8f18321aaef30638
gearmobile <gearmobile@gmail.com>
2016-10-11 16:13:29 +0300

    callback function

--- javascript/velocity/app.js
+++ javascript/velocity/app.js
@@ -1,40 +1,54 @@
 window.addEventListener( 'DOMContentLoaded', function () {
+    //
+    // VARIABLES
     let body = document.body;
     let block = document.querySelector('#anima');
     let button = document.querySelector('#get');
+
+    //
     button.style.cursor = 'pointer';
     let top = document.querySelector('#top');
     let sections = document.querySelectorAll('.section');
     let navigation = document.querySelector('.nav');
+
+    //
+    // CONFIGS
     let config = {
         width: 1000,
         opacity: .6,
         translateX: 200,
         translateY: 100
     };
+
     let physics = {
         easing: [ 500, 20 ],
         duration: 2000
     };
+
     let scroll = {
         duration: 1500,
         easing: 'swing'
     };
+
     let duration = {
         duration: 1000,
         easing: 'ease-in-out',
         delay: 100,
         loop: 2
     };
+
+    //
     button.addEventListener( 'click', function () {
         Velocity( block, config, physics );
         Velocity( block, 'reverse' );
     });
+
     // SCROLL TO TOP VELOCITY
     // --------------------------------------------------
     top.addEventListener( 'click', function () {
         Velocity( body, 'scroll', scroll );
     });
+
     // SMOOTH SCROLL VELOCITY
     // --------------------------------------------------
     navigation.addEventListener( 'click', function ( event ) {
