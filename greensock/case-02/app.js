84f3c0baa243b7e73113d9fc558b20a46fcdf8c9
gearmobile <gearmobile@gmail.com>
2016-10-15 16:08:46 +0300

    js-lesson - clean

--- greensock/case-02/app.js
+++ greensock/case-02/app.js
@@ -1,5 +1,3 @@
 window.addEventListener( 'load', function () {
     // code here ...
-    const firstElement = document.querySelector('#box-item-one');
-    TweenLite.to( firstElement, 3, { x: 200 } );
 }, false );
