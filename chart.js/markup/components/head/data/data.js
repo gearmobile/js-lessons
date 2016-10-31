5ef2ffb541fa35d629e5df247c0abc190f45b5de
gearmobile <gearmobile@gmail.com>
2016-10-31 15:00:30 +0300

    chartjs - scatter plot

--- chart.js/markup/components/head/data/data.js
+++ chart.js/markup/components/head/data/data.js
@@ -2,5 +2,17 @@ head: {
     defaults: {
         title: 'Chart.js',
         useSocialMetaTags: true
+    },
+    scatter: {
+        title: 'Chart.js - Scatter Chart',
+        useSocialMetaTags: true
+    },
+    bubble: {
+        title: 'Chart.js - Bubble Chart',
+        useSocialMetaTags: true
+    },
+    stacked: {
+        title: 'Chart.js - Stacked Chart',
+        useSocialMetaTags: true
     }
 }
--- chart.js/markup/components/scatter/scatter.js
+++ chart.js/markup/components/scatter/scatter.js
@@ -2,33 +2,41 @@ const Chart = require( 'chart.js' );
 // --------------------------------------------------
 window.addEventListener( 'load', function () {
     // ----------------------------------------------
-    let plot = document.querySelector( '#scatter' );
-    //
-    const plotData = {
-        labels: [ 'uno', 'duo', 'tre', 'quattro' ],
+    const scatterPlot = document.querySelector( '#scatter' );
+    // ----------------------------------------------
+    const scatterData = {
         datasets: [
             {
                 label: 'Scatter Chart',
-                data: [ 20, '30', 10, 40 ]
+                data: [
+                    { x: 20, y: 10 },
+                    { x: 50, y: 30 },
+                    { x: 90, y: 50 },
+                    { x: 10, y: 20 },
+                    { x: 60, y: 80 },
+                    { x: 40, y: 10 }
+                ]
             }
         ]
     };
     // ----------------------------------------------
-    const plotOptions = {
-        //showLines: false
+    const scatterOptions = {
         scales: {
-            yAxes: [
-                {
-                    stacked: true
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom',
+                ticks: {
+                    beginAtZero: true
                 }
-            ]
-        }
+            }]
+        },
+        hoverMode: 'single'
     };
     // ----------------------------------------------
-    const scatterPlot = new Chart( plot, {
-        type: 'bar',
-        data: plotData,
-        options: plotOptions
+    const scatterCanvas = new Chart( scatterPlot, {
+        type: 'line',
+        data: scatterData,
+        options: scatterOptions
     });
     // ----------------------------------------------
 }, false );
