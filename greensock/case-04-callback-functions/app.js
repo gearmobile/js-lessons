e01a227d385be1fec4ffd023f67e17eb3c2d2272
gearmobile <gearmobile@gmail.com>
2016-11-01 11:30:48 +0300

    greensock - callback functions

--- greensock/case-04-callback-functions/app.js
+++ greensock/case-04-callback-functions/app.js
@@ -1,8 +1,31 @@
 // -----------------------------------------------------------
-// ------------
+// CALLBACK FUNCTIONS: ONSTART(), ONUPDATE(), ONCOMPLETE()
 // -----------------------------------------------------------
 window.addEventListener( 'load', function () {
     // ----------------------------------------------------------
-    const elementUno = document.querySelector( '#box-item-one' );
+    let elementUno = document.querySelector( '#box-item-one' );
+    let count = 0;
+    // ----------------------------------------------------------
+    function startAnima() {
+        console.log( 'Animations started!' );
+    }
+    function progressAnima() {
+        console.log( 'Animation is in progress' );
+        //elementUno.innerHTML = count++;
+    }
+    function endAnima() {
+        console.log( 'Animation finished!' );
+        //elementUno.style.backgroundColor = 'mediumseagreen';
+        //elementUno.style.color = 'lightgoldenrodyellow';
+        //elementUno.style.borderColor = 'firebrick';
+    }
+    // ----------------------------------------------------------
+    TweenLite.to( elementUno, 4, {
+        x: 800,
+        delay: 1,
+        onStart: startAnima,
+        onUpdate: progressAnima,
+        onComplete: endAnima
+    } );
     // ----------------------------------------------------------
 }, false );
