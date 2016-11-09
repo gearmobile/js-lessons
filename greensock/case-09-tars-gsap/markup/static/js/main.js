f7533b0881ec9e1aa22401384fe2850472e98c8e
gearmobile <gearmobile@gmail.com>
2016-11-09 13:45:16 +0300

    greensock - case 10 - svg animation

--- greensock/case-09-tars-gsap/markup/static/js/main.js
+++ greensock/case-09-tars-gsap/markup/static/js/main.js
@@ -2,4 +2,5 @@
 
 // include block with GSAP
 // -------------------------------------------------
+//require( '../../components/svg-animations/svg-animations' );
 require( '../../components/block-gsap/block-gsap' );
--- greensock/case-10-tars-svg-animations/markup/components/svg-animations/svg-animations.js
+++ greensock/case-10-tars-svg-animations/markup/components/svg-animations/svg-animations.js
@@ -1,4 +1,36 @@
 // ------------------------------------------
 const gsap = require( 'gsap' );
-const $ = require( 'sizzle' );
 // ------------------------------------------
+window.addEventListener( 'load', function () {
+    // ---------------------------------------------------
+    const Timeline = gsap.TimelineLite;
+    const Back = gsap.Back;
+    const tlMain = new Timeline({ paused: true });
+    // ---------------------------------------------------
+    const btnPlay = document.querySelector( '#button-play' );
+    const btnReverse = document.querySelector( '#button-reverse' );
+    // ---------------------------------------------------
+    const box = document.querySelector( '.box' );
+    const boxHTML = document.querySelector( '.box__html' );
+    const boxSVG = document.querySelector( '.box__svg' );
+    const boxHTMLDOT = document.querySelector( '.box__html > span' );
+    const boxSVGDOT = document.querySelector( '.box__svg > circle' );
+    // ---------------------------------------------------
+    const duration = 1;
+    // ---------------------------------------------------
+    function animaPlay() {
+        tlMain.play();
+    }
+    // ---------------------------------------------------
+    function animaReverse() {
+        tlMain.reverse();
+    }
+    // --------------------------------------------------
+    tlMain
+        .to( boxHTML, duration, { x: '100%', y: '100%', ease: Back.easeOut } )
+        .to( boxSVG, duration, { x: '100%', y: '100%', ease: Back.easeOut } );
+    // ---------------------------------------------------
+    btnPlay.addEventListener( 'click', animaPlay, false );
+    btnReverse.addEventListener( 'click', animaReverse, false );
+    // ---------------------------------------------------
+}, false );
