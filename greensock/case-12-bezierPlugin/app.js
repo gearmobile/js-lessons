224240d3ec57d1ea982f97485e4659f77e7b126e
gearmobile <gearmobile@gmail.com>
2016-11-16 20:12:33 +0300

    greensock - bezier plugin

--- greensock/case-12-bezierPlugin/app.js
+++ greensock/case-12-bezierPlugin/app.js
@@ -1,5 +1,20 @@
 window.addEventListener( 'load', function () {
     // ----------------------------------------------------------
-    const firstElement = document.querySelector( '#box-item-one' );
+    const plane = document.querySelector( '#plane' );
     // ----------------------------------------------------------
+    TweenMax.to( plane, 6, {
+        bezier: {
+            values: [
+                { left: 300, top: 25 },
+                { left: 550, top: 300 },
+                { left: 300, top: 550 },
+                { left: 25,  top: 300 },
+                { left: 300, top: 25 }
+            ],
+            autoRotate: 45,
+            type: 'thru',
+            curviness: 1.4
+        },
+        ease: Linear.easeNone
+    });
 }, false );
