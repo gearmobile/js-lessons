dafefec0bd663412b11dfcc40159a7e4d8a20fb5
gearmobile <gearmobile@gmail.com>
2016-11-20 15:06:29 +0300

    greensock - matching game finished

--- greensock/case-14-matchingGame/app.js
+++ greensock/case-14-matchingGame/app.js
@@ -8,8 +8,8 @@ window.addEventListener( 'load', function () {
     // ----------------------------------------------------------
     const gameTitle = new SplitText( '.game-title' );
     const gameBoard = document.querySelector( '.game-board' );
-    let gameBoardTop = gameBoard.getBoundingClientRect().top + 1;
-    let gameBoardLeft = gameBoard.getBoundingClientRect().left + 1;
+    const gameBoardTop = gameBoard.getBoundingClientRect().top + 1;
+    const gameBoardLeft = gameBoard.getBoundingClientRect().left + 1;
     const charsNumber = gameTitle.chars.length;
     const imageItems = document.querySelectorAll( '.item' );
     const draggableIcons = document.querySelectorAll( '.draggable-item' );
@@ -17,6 +17,9 @@ window.addEventListener( 'load', function () {
     const totalScore = 6;
     let totalHits = 0;
     const gameContainer = document.querySelector( '.game-board' );
+    const gameOver = document.querySelector( '.game-over' );
+    const gameMiss = document.querySelector( '.game-miss' );
+    const gameHit = document.querySelector( '.game-hit' );
 
     // timeline instances
     // ----------------------------------------------------------
@@ -25,6 +28,10 @@ window.addEventListener( 'load', function () {
     // set icons initial properties
     // ----------------------------------------------------------
     TweenMax.set( imageItems, { scale: 0, opacity: 0 } );
+    TweenMax.set( gameOver, { scale: 0 } );
+    TweenMax.set( gameMiss, { scale: 0, autoAlpha: 0 } );
+    TweenMax.set( gameHit, { scale: 0, autoAlpha: 0 } );
+
 
     // split text plugin
     // ----------------------------------------------------------
@@ -40,6 +47,45 @@ window.addEventListener( 'load', function () {
         }
     }
 
+    // hide try again modal window
+    // ----------------------------------------------------------
+    function hidePopup() {
+        TweenMax.to( gameMiss, 1.4, { scale: 0, autoAlpha: 0, ease: Back.easeIn, delay: 0.1 } );
+    }
+
+    // show try again modal window
+    // ----------------------------------------------------------
+    function showPopup() {
+        TweenMax.to( gameMiss, 0.8, { scale: 1, autoAlpha: 1, ease: Back.easeOut, onComplete: hidePopup } );
+    }
+
+    // hide popup success
+    // ----------------------------------------------------------
+    function hidePopupSuccess() {
+        TweenMax.to( gameHit, 0.8, { scale: 0, autoAlpha: 0, ease: Back.easeOut, onComplete: finishGame } )
+    }
+
+    // show popup success
+    // ----------------------------------------------------------
+    function showPopupSuccess() {
+        TweenMax.to( gameHit, 0.8, { scale: 1, autoAlpha: 1, ease: Back.easeOut, onComplete: hidePopupSuccess } );
+    }
+
+    // finish game modal window
+    // ----------------------------------------------------------
+    function finishGame() {
+        if ( totalHits === targetItems.length ) {
+            TweenMax.to( gameOver, 2.8, { scale: 1, ease: Elastic.easeOut } );
+        }
+    }
+
+    // hide matched elements
+    // ----------------------------------------------------------
+    function hideElements( dragItem, targetItem ) {
+        totalHits++;
+        TweenMax.to( [ dragItem.target, targetItem ], 0.6, { autoAlpha: 0, onComplete: showPopupSuccess } );
+    }
+
     // make icons board draggable
     // ----------------------------------------------------------
     function initDraggableItems() {
@@ -51,13 +97,8 @@ window.addEventListener( 'load', function () {
             onPress: function () {
                 this.startX = this.x;
                 this.startY = this.y;
-                let position = $( this.target ).position();
-                let targetDimentions = this.target.getBoundingClientRect();
-                console.log( position.top + ' : ' + position.left );
-                console.log( ( this.startY - position.top ) + ' : ' + ( this.startX - position.left ) );
-                // console.log( ( targetDimentions.top - ( gameBoardDimentions.top + 1 ) ) + ' : ' + ( targetDimentions.left - ( gameBoardDimentions.left + 1 ) ) );
-                // console.log( targetDimentions.width + ' : ' + targetDimentions.height );
-                // console.log( gameBoard );
+                this.offsetTop = this.startY - ( this.target.getBoundingClientRect().top - gameBoardTop );
+                this.offsetLeft = this.startY - ( this.target.getBoundingClientRect().left - gameBoardLeft );
             },
             onDragEnd: function () {
                 let targetID = this.target.id + '-drop';
@@ -66,13 +107,23 @@ window.addEventListener( 'load', function () {
                     let itemID = item.id;
                     if ( targetID === itemID ) {
                         if ( dragItem.hitTest( item, '50%' ) ) {
-                            dragItem.y = item.getBoundingClientRect().top - gameBoardTop;
-                            dragItem.x = item.getBoundingClientRect().left - gameBoardLeft;
+                            let itemPosition = item.getBoundingClientRect();
+                            let diffTop = dragItem.offsetTop + ( itemPosition.top - gameBoardTop );
+                            let diffLeft = dragItem.offsetLeft + ( itemPosition.left - gameBoardLeft );
+                            console.log( diffTop, diffLeft );
+                            TweenMax.to( dragItem.target, 1, {
+                                x: diffLeft,
+                                y: diffTop,
+                                ease: Back.easeOut,
+                                onComplete: hideElements,
+                                onCompleteParams: [ dragItem, item ]
+                            });
                         } else {
                             TweenMax.to( dragItem.target, 0.8, {
                                 x: dragItem.startX,
                                 y: dragItem.startY,
-                                ease: Back.easeOut
+                                ease: Back.easeOut,
+                                onComplete: showPopup
                             });
                         }
                     }
