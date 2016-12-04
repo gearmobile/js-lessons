8e969f1a3b6382a54b8f8e30f02a8c20765ed6e8
gearmobile <gearmobile@gmail.com>
2016-12-04 10:12:57 +0300

    greensock - directional rotation plugin

--- greensock/case-19-directionalRotation/app.js
+++ greensock/case-19-directionalRotation/app.js
@@ -1,38 +1,64 @@
 window.addEventListener( 'load', function () {
     // ----------------------------------------------------------
-    const box = document.querySelector( '.box' );
-    const btnRun = document.querySelector( '.run' );
-    const btnReload = document.querySelector( '.reload' );
+    const boxUno = document.querySelector( '#box-uno' );
+    const boxies = document.querySelectorAll( '.box' );
+    const btnUno = document.querySelector( '#controls-uno' );
+    const btnDuo = document.querySelector( '#controls-duo' );
+    const btnTre = document.querySelector( '#controls-tre' );
+    const btnQuattro = document.querySelector( '#controls-quattro' );
+    const btnCinque = document.querySelector( '#controls-cinque' );
     // ----------------------------------------------------------
-    TweenMax.set( box, {
-        rotation: '45deg',
-        transformOrigin: 'center center'
-    });
-    // ----------------------------------------------------------
-    function fnReload() {
-        window.location.reload();
-    }
-    // ----------------------------------------------------------
-    function simpleRotation() {
-        TweenMax.to( box, 1, {
-            rotation: '360deg'
+    function oldFushion() {
+        TweenMax.to( boxUno, 4, {
+            // rotation: 360
+            rotation: -360,
+            transformOrigin: 'center center'
         });
     }
-    // ----------------------------------------------------------
-    function directionalRotationClockwise() {
-        TweenMax.to( box, 1, {
-            rotation: '360_ccw'
+    // -----------------------------------------------------------
+    function clockWise() {
+        TweenMax.to( boxUno, 4, {
+            directionalRotation: '360_cw'
         });
     }
-    // ----------------------------------------------------------
-    function directionalRotationCounterClockwise() {
-        TweenMax.to( box, 1, {
-            rotation: '360_short'
+    // -----------------------------------------------------------
+    function counterClockWise() {
+        TweenMax.to( boxUno, 4, {
+            directionalRotation: '360_ccw'
         });
     }
-    // ----------------------------------------------------------
-    // btnRun.addEventListener( 'click', simpleRotation, false );
-    btnRun.addEventListener( 'click', directionalRotationCounterClockwise, false );
-    btnReload.addEventListener( 'click', fnReload, false );
-    // ----------------------------------------------------------
+    // -----------------------------------------------------------
+    function complexRotation() {
+        TweenMax.to( boxUno, 4, {
+            directionalRotation: {
+                rotationX: '180_cw',
+                rotationY: '180_ccw',
+                useRadian: true
+            }
+        });
+    }
+    // -----------------------------------------------------------
+    function randomRotation() {
+        let flag = true;
+        let randomAngle;
+        let duration;
+        boxies.forEach( function ( item ) {
+            if ( flag ) {
+                randomAngle = Math.floor( Math.random() * 720 ) + '_cw';
+            } else {
+                randomAngle = Math.floor( Math.random() * 720 ) + '_ccw';
+            }
+            duration = Math.floor( Math.random() * 10 );
+            TweenMax.to( item, duration, {
+                directionalRotation: randomAngle
+            });
+            flag = !flag;
+        });
+    }
+    // -----------------------------------------------------------
+    btnUno.addEventListener( 'click', oldFushion, false );
+    btnDuo.addEventListener( 'click', clockWise, false );
+    btnTre.addEventListener( 'click', counterClockWise, false );
+    btnQuattro.addEventListener( 'click', complexRotation, false );
+    btnCinque.addEventListener( 'click', randomRotation, false );
 }, false );
