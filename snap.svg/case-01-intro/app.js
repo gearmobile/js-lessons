4d1847208d4f32bbf5044a002e85d8da4fafd1cd
gearmobile <gearmobile@gmail.com>
2016-12-12 21:36:54 +0300

    snap.svg

--- snap.svg/case-01-intro/app.js
+++ snap.svg/case-01-intro/app.js
@@ -1,6 +1,7 @@
 
 window.addEventListener( 'load', function () {
     const sample = Snap( '#example' );
-    const circle = sample.circle( 100, 100, 50 );
+    console.log( sample );
+    const circle = sample.circle( 200, 200, 200 );
 }, false );
 
--- snap.svg/case-02-shapes/app.js
+++ snap.svg/case-02-shapes/app.js
@@ -1,12 +1,15 @@
 
 window.addEventListener( 'load', function () {
+
+    // -----------------------------
     const snapObject = Snap( 600, 400 );
 
-    // circle object
+    // CIRCLE OBJECT
     // -----------------------------
     const circle = snapObject.circle( 100, 100, 50 );
 
-    // rectangle object
+    // RECTANGLE OBJECT
     // -----------------------------
     const rect = snapObject.rect( 200, 50, 100, 100 );
+
 }, false );
